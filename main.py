9e5ae99a3bd70175efbff9c3406a24dc579628d2
WangZiXi <101445217+WangZiXi@users.noreply.github.com>
2024-04-05 23:15:41 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+
+data = pd.read_excel('积分人员对照表.xlsx')
+
+data = data.drop_duplicates()
+
+col1,col2 = st.columns(2)
+
+name = col1.text_input('请输入查询的姓名')
+
+name_in = col2.text_input('请输入录入的姓名')
+page_in = col2.text_input('请输入录入的页数')
+in_btn = col2.button('录入')
+
+submit_btn = col1.button('查询')
+
+if submit_btn:
+    filtered_df = data[data['姓名'].str.contains(name)]
+    col1.dataframe(filtered_df,hide_index=True)
+
+if in_btn:
+    if name_in and page_in:
+        new_df = data.append({'姓名':name_in,'页数':page_in},ignore_index=True)
+        new_df.to_excel('积分人员对照表.xlsx',index=False)
+        st.success('录入成功')
+    else:
+        st.warning('请输入姓名和电话')
+    
+    
